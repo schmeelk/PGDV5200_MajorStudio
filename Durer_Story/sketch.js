c59ead23368f3d1645a10d925c0ebccf70e54c17
schmeelk <sschmeelk@pace.edu>
2018-04-01 21:51:13 -0400

    CSV Update

--- Durer_Story/sketch.js
+++ Durer_Story/sketch.js
@@ -1,28 +1,40 @@
+var durImg;  // Declare variable 'img'.
+var table;
 var tableDurer;
+var currentYear;
 var durerRows;
+var itemRows;
 var fr = 10;
-var drawTrueMax = 1;
+var drawTrueMax = 2;
 var drawTrue = 0;
 var margin = 150;
 var file;
 var width = 250;
 var height = 250;
 var yearText;
-var durTextYear = [];
+var allText = [];
+var start = 1480;
 var allDurImages ={};
+var allMetImages ={};
 var durStart = 1480;
 var durEnd = 1526;
 var durAge = 9;
+//var div = createDiv('').size(400, 50 );
+var div;
 
 function preload() {
-  tableDurer = loadTable('./durer.csv', 'csv', 'header');
-  console.log("Loading Dur Images colums[" + tableDurer.getColumnCount() + "] rows [" + tableDurer.getRowCount() + "]");
+  tableDurer = loadTable('MetObjects_Durer_Final.csv', 'csv', 'header');
   prepareDurImages();
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight * 100);
-  //frameRate(fr);
+  //createCanvas(720, 400);
+  var height = 2000;
+  createCanvas(windowWidth, height);
+  frameRate(fr);
+  console.log("Here we are.");
+  prepareDurImages();
+  draw();
 }
 
 
@@ -42,30 +54,35 @@ function prepareDurImages(){
      console.log("Found year ["+ year + "] durerAllYearImages.length: " + durerAllYearImages.length);
      allDurImages[year] = durerAllYearImages;
     }
+
+  draw();
 }
 
 
-function draw() {
-  //if(drawTrue >= drawTrueMax){
-  //   return;
-  //}
-  var durImg;
-  console.log("DrawTrue count: " + String(drawTrue));
-  var durImageWidth = 10; 
-  var placeDiv = 30;
-  var haveDur = allDurImages[currentYear];
-  if(haveDur == -1){
-     console.log("No Dur image this year");
-  }else{ 
-     durImg = allDurImages[currentYear];
-     console.log("Drawing durer year image");
-     image(durImg, 50, 125);
-     console.log(durImg);
-     durImageWidth = durImg.width;
-     placeDiv = durImageWidth + 100;
-     console.log("Drawing durer year image");
-  }
 
+function draw() {
+  if(drawTrue >= drawTrueMax){
+     return;
+  }
+  console.log("DrawTrue count: " + String(drawTrue));
   drawTrue = drawTrue + 1;
+
+  var durImageY = 225;
+  var durImageWidth = 10;
+  for (var year = durStart; year < durEnd; year++) {
+  var haveDur = allDurImages[year];
+    if(haveDur.length == 0){
+       console.log("No Dur image this year");
+    }else{
+     durImg = allDurImages[year];
+     for(var i = 0; i < durImg.length; i++){
+       image(durImg[i], 200, durImageY, durImg[i].width/2, durImg[i].height/2);
+       console.log(durImg[i]);
+       durImageY = durImageY + durImg[i].height/2 + 10;
+      }
+    }
+  }
+  console.log("Finished draw");
+  
 }
 
