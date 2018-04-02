2198736572e091432ed8db473255128fd3e0bb5b
schmeelk <sschmeelk@pace.edu>
2018-04-02 13:12:01 -0400

    Load Durer Images

--- Durer_Story/sketch.js
+++ Durer_Story/sketch.js
@@ -1,11 +1,12 @@
 var durImg;  // Declare variable 'img'.
+var durImgT;  // Declare variable 'img'.
 var table;
 var tableDurer;
 var currentYear;
 var durerRows;
 var itemRows;
 var fr = 10;
-var drawTrueMax = 2;
+var drawTrueMax = 15;
 var drawTrue = 0;
 var margin = 150;
 var file;
@@ -24,21 +25,20 @@ var div;
 
 function preload() {
   tableDurer = loadTable('MetObjects_Durer_Final.csv', 'csv', 'header');
-  prepareDurImages();
 }
 
 function setup() {
   //createCanvas(720, 400);
+  prepareDurImages();
   var height = 2000;
   createCanvas(windowWidth, height);
   frameRate(fr);
   console.log("Here we are.");
-  prepareDurImages();
-  draw();
+  
 }
 
-
 function prepareDurImages(){
+   var y = 0;
    console.log("Loading Dur Images colums[" + tableDurer.getColumnCount() + "] rows [" + tableDurer.getRowCount() + "]");
    for (var year = durStart; year < durEnd; year++) {
      var durerRows = [];
@@ -49,13 +49,13 @@ function prepareDurImages(){
      for(k=0; k < durerRows.length; k++){
        var toget = durerRows[k];
        var eImg = loadImage("images/"+ toget.get(1));  // Load the image
+       durImgT = eImg;
        append(durerAllYearImages, eImg);
      }
      console.log("Found year ["+ year + "] durerAllYearImages.length: " + durerAllYearImages.length);
      allDurImages[year] = durerAllYearImages;
     }
 
-  draw();
 }
 
 
@@ -70,19 +70,23 @@ function draw() {
   var durImageY = 225;
   var durImageWidth = 10;
   for (var year = durStart; year < durEnd; year++) {
-  var haveDur = allDurImages[year];
+    var haveDur = allDurImages[year];
     if(haveDur.length == 0){
        console.log("No Dur image this year");
     }else{
      durImg = allDurImages[year];
+     console.log("Number of Dur images this year "+durImg.length);
      for(var i = 0; i < durImg.length; i++){
-       image(durImg[i], 200, durImageY, durImg[i].width/2, durImg[i].height/2);
-       console.log(durImg[i]);
-       durImageY = durImageY + durImg[i].height/2 + 10;
+       var img = durImg[i];
+       image(durImg[i], 200, durImageY, durImg[i].width/5, durImg[i].height/5);
+       //image(img, 0,0);
+       console.log(img);
+       durImageY = durImageY + img.height/5 + 10;
       }
     }
   }
-  console.log("Finished draw");
-  
+  console.log("Image issue");
+  //image(durImgT, 0, 0);
+
 }
 
